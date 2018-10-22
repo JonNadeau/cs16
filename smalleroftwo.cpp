961015e106dd65eaa7ce8848c9ee5f2b329500dd
Jon Nadeau <jonathannadeau@cs.ucsb.edu>
2018-10-21 19:10:23 -0700

    lab02

---
+++ smalleroftwo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;main() {//int?
+	int a, b;
+	cout << "Enter two numbers:";
+	cin >> a;
+	cin >> b;
+	if (a < b)
+		cout << a;//return?
+	else
+		cout << b;//return?
+	cout << endl;
+}
